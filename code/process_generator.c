2b99dabbda905cc771980d022dad1745301fc693
AhmedMAyoub <52310517+AhmedMAyoub@users.noreply.github.com>
2021-06-13 18:15:02 +0200

    Added input file reading

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,20 +1,25 @@
 #include "headers.h"
 #include <stdio.h>
+#include <string.h>
 
-struct process {
+struct process
+{
     int id;
     int arrTime;
     int runTime;
     int waitTime;
     int totalTime;
     int remainingTime;
+    int priority;
+    char status[50];
 };
 
 int processCount = 0;
 
 void clearResources(int);
-void readInputFile(FILE* fp, char*fileName, struct process* processes);
-void countProcesses(FILE* fp, char*fileName);
+void readInputFile(FILE *fp, char *fileName, struct process *processes);
+void countProcesses(FILE *fp, char *fileName);
+void split_string(char *line, struct process *process);
 
 int main(int argc, char *argv[])
 {
@@ -23,16 +28,22 @@ int main(int argc, char *argv[])
 
     // 1. Read the input files.
     FILE *fp;
-    char* fileName = "processes.txt";
+    char *fileName = "processes.txt";
     countProcesses(fp, fileName);
-    struct process* processes[processCount];
-
+    struct process processesArr[processCount];
+    readInputFile(fp, fileName, processesArr);
     printf("%d\n", processCount);
+    for (int i=0; i<processCount; i++) {
+        printf("ID of process is %d\n", processesArr[i].id);
+        printf("ArrTime of process is %d\n", processesArr[i].arrTime);
+        printf("RunTime of process is %d\n", processesArr[i].runTime);
+        printf("Priority of process is %d\n\n", processesArr[i].priority);
+    }
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
     // initClk();
-    // To get time use this function. 
+    // To get time use this function.
     // int x = getClk();
     // printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
@@ -48,37 +59,63 @@ void clearResources(int signum)
     kill(SIGKILL, getpid());
 }
 
-void readInputFile(FILE* fp, char*fileName, struct process* processes) {
+void readInputFile(FILE *fp, char *fileName, struct process *processes)
+{
+    int num =0;
     fp = fopen(fileName, "r");
-    char chr = fgetc(fp);
-    while (!feof(fp))
+    if (fp == NULL)
     {
-        if (chr == '#')
-        {
-            while (chr != '\n'){
-                chr = fgetc(fp);
-            }
-            chr = fgetc(fp);
-        }
-        //Count whenever new line is encountered
-        if (chr == '\n')
-        {
-            chr = fgetc(fp);
-        }
-        //take next character from file.
-        chr = fgetc(fp);
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    }
+    char string[100];
+    fgets(string, 100, fp);
+    while (fgets(string, 100, fp) != NULL) {
+        split_string(string, &processes[num]);
+        num++;
     }
     fclose(fp); //close file.
 }
 
-void countProcesses(FILE* fp, char*fileName) {  //function counts number of processes
+void split_string(char *line, struct process *process) {
+
+    const char delimiter[] = "\t";
+    char *tmp;
+    tmp = strtok(line, delimiter);
+    if (tmp == NULL)
+    return;
+    process->id = atoi(tmp);
+    for (int i=0;i<3;i++) {
+        tmp = strtok(NULL, delimiter);
+        if (tmp == NULL)
+            break;
+        if (i ==0){
+        process->arrTime = atoi(tmp);
+        }
+        if (i ==1){
+        process->runTime = atoi(tmp);
+        }
+        if (i ==2){
+        process->priority = atoi(tmp);
+        }
+    }
+}
+
+void countProcesses(FILE *fp, char *fileName)
+{ //function counts number of processes
     fp = fopen(fileName, "r");
+    if (fp == NULL)
+    {
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    }
     char chr = fgetc(fp);
     while (!feof(fp))
     {
         if (chr == '#')
         {
-            while (chr != '\n'){
+            while (chr != '\n')
+            {
                 chr = fgetc(fp);
             }
             chr = fgetc(fp);
