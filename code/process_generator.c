505b906f2f808e40142598e577f7da04df0f5930
AhmedMAyoub <52310517+AhmedMAyoub@users.noreply.github.com>
2021-06-13 13:54:02 +0200

    Added function to count processes

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,28 +1,68 @@
 #include "headers.h"
+#include <stdio.h>
+
+struct process {
+    int id;
+    int arrTime;
+    int runTime;
+    int waitTime;
+    int totalTime;
+    int remainingTime;
+};
+
+int processCount = 0;
 
 void clearResources(int);
-void readInputFile();
+int readInputFile();
+void countProcesses(FILE* fp, char*fileName);
 
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
+
     // 1. Read the input files.
+    FILE *fp;
+    char* fileName = "processes.txt";
+    countProcesses(fp, fileName);
+    printf("%d\n", processCount);
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
-    initClk();
+    // initClk();
     // To get time use this function. 
-    int x = getClk();
-    printf("Current Time is %d\n", x);
+    // int x = getClk();
+    // printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
     // 6. Send the information to the scheduler at the appropriate time.
     // 7. Clear clock resources
-    destroyClk(true);
+    // destroyClk(true);
 }
 
 void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
+}
+
+int readInputFile() {
+    
+}
+
+void countProcesses(FILE* fp, char*fileName) {  //function counts number of lines in file and subtracts one for header file to count proccesses
+    fp = fopen(fileName, "r");
+    if(fp == NULL){
+        printf("Could not open file %s",fileName);
+    }
+    else {
+        char ch;
+        while(!feof(fp)){
+            ch = fgetc(fp);
+            if (ch == '\n'){
+                processCount++;
+            }
+        }
+        processCount--;
+    }
+    fclose(fp);
 }
