406aaa2729c837c39174ec05f5e373b441166c57
AhmedMAyoub <52310517+AhmedMAyoub@users.noreply.github.com>
2021-06-13 19:37:21 +0200

    Update process_generator.c

--- code/process_generator.c
+++ code/process_generator.c
@@ -29,18 +29,41 @@ int main(int argc, char *argv[])
     // 1. Read the input files.
     FILE *fp;
     char *fileName = argv[1];
-    int schedAlgo = atoi(argv[3]); // Here  we take the number of the scheduling algorithm
-    int algoParams = atoi(argv[5]);
-    printf("Sched Algo number %d\n", schedAlgo);
-    printf("Algo Parameters %d\n", algoParams);
     countProcesses(fp, fileName);
     struct process processesArr[processCount];
     readInputFile(fp, fileName, processesArr);
     printf("Process Count %d\n", processCount);
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
+    int schedAlgo = atoi(argv[3]); // Here  we take the number of the scheduling algorithm
+    int algoParams = atoi(argv[5]);
+    printf("Sched Algo number %d\n", schedAlgo);
+    printf("Algo Parameters %d\n", algoParams);
     // 3. Initiate and create the scheduler and clock processes.
+    int clock_process = fork();
+    if (clock_process == 0) //code to be executed if you are the clock child
+    {
+        char *args[] = {"./clk.out", NULL}; // using this to initialize clk.c
+        printf("initializing clock\n");
+        execvp(args[0], args);
+    }
+    else
+    { //code for process_generator
+        key_t msgQScheduler;
+        int rec_val, msgQSched_id;
+        msgQScheduler = ftok("keyfile.txt", 33); // create unique key
+        msgQSched_id = msgget(msgQScheduler, 0666 | IPC_CREAT);
+        int schedulerPId = fork();
+        if (schedulerPId == 0)  // code if i am the scheduler child
+        {
+            
+        }
+        else    //code if i am the process_generator
+        {
+
+        }
+    }
     // 4. Use this function after creating the clock process to initialize clock.
-    // initClk();
+    initClk();
     // To get time use this function.
     // int x = getClk();
     // printf("Current Time is %d\n", x);
@@ -59,7 +82,7 @@ void clearResources(int signum)
 
 void readInputFile(FILE *fp, char *fileName, struct process *processes)
 {
-    int num =0;
+    int num = 0;
     fp = fopen(fileName, "r");
     if (fp == NULL)
     {
@@ -68,33 +91,39 @@ void readInputFile(FILE *fp, char *fileName, struct process *processes)
     }
     char string[100];
     fgets(string, 100, fp);
-    while (fgets(string, 100, fp) != NULL) {
+    while (fgets(string, 100, fp) != NULL)
+    {
         split_string(string, &processes[num]);
         num++;
     }
     fclose(fp); //close file.
 }
 
-void split_string(char *line, struct process *process) {
+void split_string(char *line, struct process *process)
+{
 
     const char delimiter[] = "\t";
     char *tmp;
     tmp = strtok(line, delimiter);
     if (tmp == NULL)
-    return;
+        return;
     process->id = atoi(tmp);
-    for (int i=0;i<3;i++) {
+    for (int i = 0; i < 3; i++)
+    {
         tmp = strtok(NULL, delimiter);
         if (tmp == NULL)
             break;
-        if (i ==0){
-        process->arrTime = atoi(tmp);
+        if (i == 0)
+        {
+            process->arrTime = atoi(tmp);
         }
-        if (i ==1){
-        process->runTime = atoi(tmp);
+        if (i == 1)
+        {
+            process->runTime = atoi(tmp);
         }
-        if (i ==2){
-        process->priority = atoi(tmp);
+        if (i == 2)
+        {
+            process->priority = atoi(tmp);
         }
     }
 }
